945256a0bcac78688fadb603f1f4bd5b3debcd8e
CaseDuncan <oluochdancun19@gmail.com>
2022-08-18 18:53:13 +0300

    sql queries

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive boolean
+
+)
---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO  bears(name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 5, "M", "Brown", "High", true);
+INSERT INTO  bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 7, "M", "White", "low", false);
+INSERT INTO  bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 10, "F", "Black", "low", true);
+INSERT INTO  bears(name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 20, "M", "Black", "high", true);
+INSERT INTO  bears(name, age, sex, color, temperament, alive) VALUES("Melissa", 15, "F", "White", "high", true);
+INSERT INTO  bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 13, "M", "White", "low", true);
+INSERT INTO  bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 13, "F", "White", "low", true);
